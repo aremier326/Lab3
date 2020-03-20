876374adcfca8135474e0fe342ba3ebfc773e6a5
atem_johnson <user@address.com>
2020-03-20 17:05:16 +0200

    - First Commit

---
+++ src/com/lab3/main/task1/Formula.java
@@ -0,0 +1,22 @@
+package com.lab3.main.task1;
+
+public class Formula {
+    private double x;
+    private double y;
+    private double z;
+    private double result;
+
+    void set(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    void run() {
+
+    }
+
+    void print() {
+
+    }
+}
